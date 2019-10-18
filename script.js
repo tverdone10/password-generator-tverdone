103ea48b4575ddc52f07a7fef611d0359b1c8ba6
tverdone <tverdone10@gmail.com>
2019-10-17 21:59:37 -0700

    laid the groundwork on the js. have a working generator, but would like to make it more random. needs to be a longer password, too

---
+++ script.js
@@ -0,0 +1,64 @@
+
+var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var sym = ["!", "?", ",", ".", " ", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-"]
+
+
+console.log(num)
+
+var randomUpper = upperCase [Math.floor(Math.random()*upperCase.length)]
+var randomNum = num [Math.floor(Math.random()*num.length)]
+var randomLower = lowerCase [Math.floor(Math.random()*lowerCase.length)]
+var randomSym = sym [Math.floor(Math.random()*sym.length)]
+
+console.log(randomUpper)
+console.log(randomNum)
+console.log(randomLower)
+console.log(randomSym)
+
+var passWord = randomUpper + randomNum + randomLower + randomSym
+
+var passArray = [randomUpper, randomNum, randomLower, randomSym]
+
+console.log(passArray)
+console.log(passWord)
+
+
+
+function genErate () {
+    document.getElementById("password").innerHTML = passArray.join("")
+}
+
+
+// or
+
+// var passVariables = {
+//     capital: "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "Jones",
+//     low: "abcdefghijklmnopqrstuvwxyz",
+//     symbol: "!@#$%^&+-=",
+//     num: "1234567890"
+// }
+
+
+
+// function randomLetter (){
+//     return num[Math.floor(Math.random() * upperCase.length)]
+// }
+// // var rand = num [Math.floor(Math.random() * num.length)]
+
+// var letter = randomLetter ()
+
+// console.log(letter)
+
+
+
+
+
+
+// function () {
+//     (var i = 0, n = "capital.length" i < length; i++)
+
+// }
